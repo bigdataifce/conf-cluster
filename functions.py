b5bf4cad2797a4351a34570bd5bb1656766633c2
saulomaia2 <saulommaia@gmail.com>
2020-05-13 19:33:07 -0300

    simulacao numero de passagens para 8 sensores - mini dataset

--- functions.py
+++ functions.py
@@ -1,18 +1,13 @@
 import pandas as pd
 
 
-def filter_df(dfs, cols):
-    df_int = pd.DataFrame(columns=cols)
+def filter_df(dfs):
 
-    for i in range(0, len(dfs)):
-        df = pd.DataFrame(dfs[i].Data)
-        df["Latitude"] = dfs[i].Latitude
-        df["Longitude"] = dfs[i].Longitude
+    df = pd.DataFrame(dfs.Data)
+    df["Latitude"] = dfs.Latitude
+    df["Longitude"] = dfs.Longitude
 
-        df_int = df_int.append(df, ignore_index=True)
-
-    return df_int
-
+    return df
 
 def create_datetime(df):
     df['Datetime'] = pd.to_datetime(df.Data, format="%Y-%m-%dT%H:%M:%S.%fZ")
@@ -32,7 +27,6 @@ def convert_date(dfs):
 
     return dfs
 
-
 def append_dfs(dfs):
     df_int = pd.DataFrame(columns=dfs[0].columns)
 
@@ -49,15 +43,15 @@ def create_df_animated(df):
     lat = []
     longi = []
 
-    for day in range(1, 18):
+    for day in range(9, 18):
         df_day_month = df.loc[df["day"] == day]
         for hour_pico in range(0, 24):
             df_month_pico = df_day_month.loc[df_day_month["hour"] == hour_pico]
             df_day.append(day)
             df_hour.append(hour_pico)
             df_num.append(int(len(df_month_pico)))
-            lat.append(df_day_month["Latitude"])
-            longi.append(df_day_month["Longitude"])
+            lat.append(df_day_month.Latitude.unique()[0])
+            longi.append(df_day_month.Longitude.unique()[0])
 
     dic_cols = {"day_week": df_day, "hour": df_hour, "num_pass": df_num, "lat": lat, "long": longi}
     df_animated = pd.DataFrame(data=dic_cols)
--- geo_analysis.py
+++ geo_analysis.py
@@ -5,23 +5,42 @@ import seaborn as sns
 import numpy as np
 from functions import *
 import plotly.express as px
-import plotly.offline as py
+import random
+
 
 sns.set_style()
 
-dfx = pd.read_csv("SCOPS_OBFUSCATED_PASSAGES.tsv", sep='\t')
+dfx = pd.read_csv("/home/saulomaia/Documentos/mobit/SCOPS_OBFUSCATED_PASSAGES.tsv", sep='\t')
 sensores = dfx.Local
 
-passagens_sensor_1 = dfx.loc[((dfx['Local'] == sensores[0]))]
-passagens_sensor_2 = dfx.loc[((dfx['Local'] == sensores[1]))]
+all_dfs = []
 
-dfs_filter = filter_df([passagens_sensor_1, passagens_sensor_2], cols=["Data", "Latitude", "Longitude"])
-df_datetime = create_datetime(dfs_filter)
-dfs_convert_time = convert_date(df_datetime)
-df_animated = create_df_animated(dfs_convert_time)
+random_lats = [-7.104860, -4.520079, -6.893017, -11.235091, -11.986047, -6.270331, -6.838615, -5.943062]
+random_long = [-50.653080, -40.372369, -45.039618, -43.358975, -49.971897, -45.709057, -38.337954, -42.751896]
 
-df = px.data.gapminder()
-fig = px.scatter_geo(df_animated,
+print("[INFO] Generate the dataframes of each sensor")
+for s in range(0, len(sensores.unique())):
+    df_local = dfx.loc[((dfx['Local'] == sensores.unique()[s]))]
+    df_local.Latitude = random_lats[s]
+    df_local.Longitude = random_long[s]
+    all_dfs.append(df_local)
+
+all_filter_dfs = []
+
+print("[INFO] Filterings ...")
+for r in range(0, len(all_dfs)):
+
+    dfs_filter = filter_df(all_dfs[r])
+    df_datetime = create_datetime(dfs_filter)
+    dfs_convert_time = convert_date(df_datetime)
+    df_animated = create_df_animated(dfs_convert_time)
+    all_filter_dfs.append(df_animated)
+
+join_df = append_dfs(all_filter_dfs)
+join_df = join_df.replace(0, 1)
+
+
+fig = px.scatter_geo(join_df,
                      lat="lat", lon="long",
                      color="num_pass",
                      hover_name="day_week",
@@ -30,9 +49,10 @@ fig = px.scatter_geo(df_animated,
                      animation_frame="day_week",
                      animation_group="hour",
                      scope="south america",
-                     #width=800, height=800,
+                     #projection="natural earth",
+                     width=700, height=700,
+                     #center={"lat": -4.795678, "lon": -40.717659},
                      labels={"hour": "Hour of the day", "num_pass": "Number of Passes", "day_week": "Day of the Month"},
-                     size_max=50)
+                     size_max=25
+                     )
 fig.show()
-py.plot(fig, filename='file.html')
-
